a4fcefe9b89b200a910fdee41570d0bf616523ee
sharmatanikesh <sharmatanikesh@gmail.com>
2025-04-17 22:16:41 +0530

    feat:customization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmSanes = DM_Sans({
   subsets: ["latin"],
-});
+})
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body
       suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dmSanes.className}} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-    <div className="text-red-500">
-        hello page
+   <div className="m-6">
+    <Button variant={"elevated"}>I am button</Button>
+    <div className="m-6">
+    <Input placeholder="testing border"/>
     </div>
+
+    <div className="m-6">
+      <Progress value={50} />
+    </div>
+
+    <div className="m-6">
+
+      <Textarea placeholder="testing border"/>
+    </div>
+      
+   </div>
   );
 }
