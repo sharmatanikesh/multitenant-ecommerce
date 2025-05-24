72a4c5ceb5d16dd8596e5787d526d70e520f53f3
Tanikesh Sharma <sharmatanikesh@gmail.com>
2025-05-24 17:11:27 +0530

    feat: add ProductCard component and ProductListView for displaying products
    - Implemented ProductCard component to showcase individual product details including image, name, author, reviews, and price.
    - Added ProductListView component to organize and display a list of products with sorting and filtering options.
    - Integrated skeleton loading states for ProductList to enhance user experience during data fetching.

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,29 +1,30 @@
-import { Suspense } from "react";
 
 import { trpc, getQueryClient } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 
-import { ProductList,
-   ProductListSkeleton
-} from "@/modules/products/ui/components/product-list";
+import { ProductListView } from "@/modules/products/ui/views/product-list-view";
+import { SearchParams } from "nuqs";
+import { loadProductFilters } from "@/modules/products/search-params";
 
  interface Props{
     params:Promise<{
         subcategory?:string
-    }>
+    }>,
+     searchParams:Promise<SearchParams>
  }
  
- const Page =async  ({ params}:Props )=>{
+ const Page =async  ({ params,searchParams}:Props )=>{
       
    const {subcategory} = await params;
-    const queryClient = getQueryClient()
-    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({category:subcategory }));
+   const filters = await loadProductFilters (searchParams);
+   
+   const queryClient = getQueryClient()
+
+    void queryClient.prefetchQuery(trpc.products.getMany.queryOptions({category:subcategory,...filters}));
+
     return (
         <HydrationBoundary state={dehydrate(queryClient)}>
-         <Suspense fallback={<ProductListSkeleton/>}>
-                <ProductList category={subcategory}/>
-         </Suspense>
-     
+         <ProductListView category={subcategory}/>
         </HydrationBoundary>
     )
  }
--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,94 +1,107 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
 import { z } from "zod";
 import type { Sort, Where } from "payload";
- 
-import { Category } from "@/payload-types";
+
+import { DEFAULT_LIMIT } from "@/constants";
+import { Category, Media } from "@/payload-types";
 import { sortValues } from "../search-params";
 
-export const productsRouter= createTRPCRouter({
-    getMany:baseProcedure.input(
-        z.object({
-            category:z.string().nullable().optional(),
-            minPrice :z.string().nullable().optional(),
-            maxPrice :z.string().nullable().optional(),
-            tags:z.array(z.string()).nullable().optional(),
-            sort:z.enum(sortValues).nullable().optional(),
-        })
-    ).query(async({ctx,input})=>{
-      const where :Where={}
+export const productsRouter = createTRPCRouter({
+  getMany: baseProcedure
+    .input(
+      z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(DEFAULT_LIMIT),
+        category: z.string().nullable().optional(),
+        minPrice: z.string().nullable().optional(),
+        maxPrice: z.string().nullable().optional(),
+        tags: z.array(z.string()).nullable().optional(),
+        sort: z.enum(sortValues).nullable().optional(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const where: Where = {};
 
-      let sort:Sort="-createdAt"
+      let sort: Sort = "-createdAt";
 
-      if(input.sort==="curated"){
-        sort="-createdAt"
+      if (input.sort === "curated") {
+        sort = "-createdAt";
       }
-       if(input.sort==="trending"){
-        sort="name"
+      if (input.sort === "trending") {
+        sort = "name";
       }
-       if(input.sort==="hot_and_new"){
-        sort="+createdAt"
+      if (input.sort === "hot_and_new") {
+        sort = "+createdAt";
       }
 
-      if(input.minPrice && input.maxPrice){
-        where.price={
-          greater_than_equal:input.minPrice,
-          less_than_equal:input.maxPrice
-        }
-      }else if(input.minPrice){
-        where.price={
-          greater_than_equal:input.minPrice,
-        }
-      }else if(input.maxPrice){
-          where.price={
-          less_than_equal :input.maxPrice
+      if (input.minPrice && input.maxPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+          less_than_equal: input.maxPrice,
+        };
+      } else if (input.minPrice) {
+        where.price = {
+          greater_than_equal: input.minPrice,
+        };
+      } else if (input.maxPrice) {
+        where.price = {
+          less_than_equal: input.maxPrice,
+        };
+      }
+
+      if (input.category) {
+        const categoriesData = await ctx.db.find({
+          collection: "categories",
+          limit: 1,
+          depth: 1, // Populate subcategories, subcategories[0] is type of category
+          pagination: false,
+          where: {
+            slug: {
+              equals: input.category,
+            },
+          },
+        });
+
+        const formattedData = categoriesData.docs.map((doc) => ({
+          ...doc,
+          subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+            // because of the depth 1 we are confident doc will be a type of Category
+            ...(doc as Category),
+            subcategories: undefined,
+          })),
+        }));
+
+        const subcategoriesSlug = [];
+        const parentCategory = formattedData[0];
+
+        if (parentCategory) {
+          subcategoriesSlug.push(
+            ...parentCategory.subcategories.map(
+              (subcategory) => subcategory.slug
+            )
+          );
         }
       }
-    
-        if(input.category){
-          const categoriesData = await ctx.db.find({
-            collection:"categories",
-            limit:1,
-            depth:1, // Populate subcategories, subcategories[0] is type of category
-            pagination:false,
-            where:{
-              slug:{
-                equals:input.category
-              }
-            }
-          })
 
-          const formattedData= categoriesData.docs.map((doc) => ({
-                      ...doc,
-                      subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
-                        // because of the depth 1 we are confident doc will be a type of Category
-                        ...(doc as Category),
-                        subcategories:undefined
-                      })),
-                    }));
-          
-
-          const subcategoriesSlug =[]
-          const parentCategory = formattedData[0];
-
-          if(parentCategory){
-             subcategoriesSlug.push(
-              ...parentCategory.subcategories.map((subcategory)=>subcategory.slug )
-             )
-             
-          }
-        }
-
-        if(input.tags && input.tags.length >0){
-          where["tags.name"]={
-            in:input.tags
-          } 
-        }
-          const data = await ctx.db.find({
-            collection:"products",
-            depth:1, // Populate categories and images 
-            where,
-            sort 
-          });
-        return data;
+      if (input.tags && input.tags.length > 0) {
+        where["tags.name"] = {
+          in: input.tags,
+        };
+      }
+      const data = await ctx.db.find({
+        collection: "products",
+        depth: 1, // Populate categories and images
+        where,
+        sort,
+        page: input.cursor,
+        limit: input.limit,
+      });
+      return {
+        ...data,
+        docs: data.docs.map((doc) => ({
+          ...doc,
+          image: doc.image as Media | null,
+        })),
+      };
     }),
-})
+});
